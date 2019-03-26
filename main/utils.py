1faeb4cc80a3b9f6e7fb2f07c17c1ba3ff33f8e8
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2019-03-26 01:03:31 +0100

    setting stuff up

---
+++ main/utils.py
@@ -0,0 +1,17 @@
+import math
+import cmath
+
+def get_complex_roots(cmplx_num, root_degree):
+    ''' returns a list of roots of a complex number
+        Example:
+        roots = get_complex_roots(-2j, 2)
+        [(1.0000000000000002-1j), (-1+1.0000000000000002j)]
+    '''
+    roots_list = []
+    modulus = math.sqrt(cmplx_num.real ** 2 + cmplx_num.imag ** 2)
+    modulus_root = modulus ** (1/root_degree)
+    phase = cmath.phase(cmplx_num)
+    for i in range(root_degree):
+        root = cmath.rect(modulus_root, (phase + 2 * i * math.pi)/root_degree)
+        roots_list.append(root)
+    return roots_list
